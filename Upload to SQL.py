5dd961555b15ef18c9fb4ac9773cac65b79469e1
AdamKeeley <a.keeley@leeds.ac.uk>
2021-02-11 17:56:43 +0000

    commit

--- Upload to SQL.py
+++ Upload to SQL.py
@@ -17,12 +17,15 @@ sql_database = 'P0223'
 sql_schema = 'dbo'
 sql_table = 'covid19_tpp_gp_clinical'
 chunksize = 1000000
+delimiter = '\t'
 
-for chunk in pd.read_csv(file, sep='\t', chunksize=chunksize):
+for chunk in pd.read_csv(file, sep=delimiter, chunksize=chunksize):
     conn = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=" + sql_server + ";DATABASE=" + sql_database + ";Trusted_Connection=yes")
     df = pd.DataFrame(chunk)
     df.rename(columns=df.iloc[0])
+    
     df.event_dt = pd.to_datetime(df.event_dt, utc = True)
+    
     engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % conn, fast_executemany=True)
     df.to_sql(sql_table, con = engine, if_exists = "append", schema = sql_schema, index = False)
 df = df.iloc[0:0]
